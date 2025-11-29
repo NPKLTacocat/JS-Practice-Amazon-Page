56fa31057ad707dc9460d35698cd1e90ea4d60dd
Khang Tran <116851477+NPKLTacocat@users.noreply.github.com>
2025-11-29 14:07:03 -0500

    Implemented quantity selector when adding to cart

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -28,7 +28,7 @@ products.forEach((product) => {
 
     <div class="product-price">$${(product.priceCents / 100).toFixed(2)}</div>
     <div class="product-quantity-container">
-      <select>
+      <select class='js-quantity-selector-${product.id}'>
         <option selected value="1">1</option>
         <option value="2">2</option>
         <option value="3">3</option>
@@ -62,6 +62,9 @@ document.querySelector(".js-product-grid").innerHTML = productsHTML;
 document.querySelectorAll(".js-add-to-cart").forEach((button) =>
   button.addEventListener("click", () => {
     const productId = button.dataset.productId;
+    const productQuantity = Number(
+      document.querySelector(`.js-quantity-selector-${productId}`).value
+    );
 
     let matchedItem;
     cart.forEach((item) => {
@@ -71,11 +74,11 @@ document.querySelectorAll(".js-add-to-cart").forEach((button) =>
     });
 
     if (matchedItem) {
-      matchedItem.quantity++;
+      matchedItem.quantity += productQuantity;
     } else {
       cart.push({
         productId,
-        quantity: 1,
+        quantity: productQuantity,
       });
     }
 
