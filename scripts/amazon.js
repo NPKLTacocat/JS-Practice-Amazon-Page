2d0535ee5a686d294c2f35a55f3eef75ce3b22ba
Khang Tran <116851477+NPKLTacocat@users.noreply.github.com>
2025-11-29 13:20:23 -0500

    implemented add to cart button

--- scripts/amazon.js
+++ scripts/amazon.js
@@ -49,7 +49,9 @@ products.forEach((product) => {
       Added
     </div>
 
-    <button class="add-to-cart-button button-primary">Add to Cart</button>
+    <button class="add-to-cart-button button-primary js-add-to-cart" data-product-id = "${
+      product.id
+    }">Add to Cart</button>
   </div>
   `;
 });
@@ -57,3 +59,25 @@ products.forEach((product) => {
 console.log(productsHTML);
 
 document.querySelector(".js-product-grid").innerHTML = productsHTML;
+document.querySelectorAll(".js-add-to-cart").forEach((button) =>
+  button.addEventListener("click", () => {
+    const productId = button.dataset.productId;
+
+    let matchedItem;
+    cart.forEach((item) => {
+      if (item.productId === productId) {
+        matchedItem = item;
+      }
+    });
+
+    if (matchedItem) {
+      matchedItem.quantity++;
+    } else {
+      cart.push({
+        productId,
+        quantity: 1,
+      });
+    }
+    console.log(cart);
+  })
+);
